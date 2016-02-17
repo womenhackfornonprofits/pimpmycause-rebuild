88c1273a4c765293ea9735e02f9d009eb6d2050f
Rhonda Impey <rhonda_impey@hotmail.co.uk>
2016-02-17 16:49:24 +0000

    PIMP-73 change the 3D cubes to rotate on a timer rather than a hover event

--- frontend/src/scripts/scripts.js
+++ frontend/src/scripts/scripts.js
@@ -1,5 +1,27 @@
 $(document).foundation();
 
-$(document).ready(function() {
+$(window).on("load", function() {
+    
+    // rotating the cubes on the front hero panel using a timer
+    
+    var x = true;
+    
+    var topLeft = $(".cube").eq(0);
+    var topRight = $(".cube").eq(1);
+    var bottomLeft = $(".cube").eq(2);
+    var bottomRight = $(".cube").eq(3);
+    
+    setInterval(function () {
+        
+        if (x) {
+            topLeft.toggleClass("active");
+            bottomRight.toggleClass("active");
+        } else {
+            topRight.toggleClass("active");
+            bottomLeft.toggleClass("active");
+        }
+        x = !x;
+
+    }, 2000);
     
 });
